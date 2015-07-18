b0e11f8d508f77badf2fa294b6f7e925dcb3aee9
rs7 <rs7xxx@gmail.com>
2015-07-18 15:59:22 +0600

    Добавлен пользователь БД #9
    resolve #9

--- db/src/schema.sql
+++ db/src/schema.sql
@@ -1,1 +1,6 @@
-CREATE DATABASE `next_please` DEFAULT CHARACTER SET utf8;
+CREATE DATABASE `np_data` DEFAULT CHARACTER SET utf8;
+
+GRANT ALL ON `np_data`.* TO 'np_server'@'localhost' IDENTIFIED BY '123';
+# GRANT автоматически создаёт пользователя, если он ещё не создан.
+# Не используется CREATE USER, потому что MySQL не поддерживает CREATE USER IF NOT EXIST.
+# Начиная с MySQL 5.7.6 данное поведение GRANT считается устаревшим
