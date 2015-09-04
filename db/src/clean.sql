242bd950b2762cbaa68466e82ea0336edf95113c
Roman Suslin <suslin@azoft.com>
2015-09-04 23:18:58 +1000

    Структура БД для Итерации_1 (resolve #13)
    Добавлена таблица user;
    Добавлена таблица relation;
    Добавлена таблица transition;
    Добавлена таблица change;

--- db/src/clean.sql
+++ db/src/clean.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS `np_data`;
-
 GRANT USAGE ON *.* TO 'np_server'@'localhost' IDENTIFIED BY 'clean.sql';
 DROP USER 'np_server'@'localhost';
-# GRANT автоматически создаёт пользователя, если он ещё не создан.
-# Это необходимо, потому что MySQL не поддерживает DROP USER IF EXIST.
-# Начиная с MySQL 5.7.6 данное поведение GRANT считается устаревшим
+-- GRANT автоматически создаёт пользователя, если он ещё не создан.
+-- Это необходимо, потому что MySQL не поддерживает DROP USER IF EXIST.
+-- Начиная с MySQL 5.7.6 данное поведение GRANT считается устаревшим
+
+DROP DATABASE IF EXISTS `np_data`;
--- db/src/schema.sql
+++ db/src/schema.sql
@@ -1,6 +1,33 @@
+GRANT ALL ON `np_data`.* TO 'np_server'@'localhost' IDENTIFIED BY '123';
+-- GRANT автоматически создаёт пользователя, если он ещё не создан.
+-- Не используется CREATE USER, потому что MySQL не поддерживает CREATE USER IF NOT EXIST.
+-- Начиная с MySQL 5.7.6 данное поведение GRANT считается устаревшим
+
 CREATE DATABASE `np_data` DEFAULT CHARACTER SET utf8;
 
-GRANT ALL ON `np_data`.* TO 'np_server'@'localhost' IDENTIFIED BY '123';
-# GRANT автоматически создаёт пользователя, если он ещё не создан.
-# Не используется CREATE USER, потому что MySQL не поддерживает CREATE USER IF NOT EXIST.
-# Начиная с MySQL 5.7.6 данное поведение GRANT считается устаревшим
+USE `np_data`;
+
+CREATE TABLE `user` (
+	`id` INT UNSIGNED NOT NULL KEY
+);
+
+CREATE TABLE `relation` (
+	`id` TINYINT UNSIGNED NOT NULL KEY
+);
+
+CREATE TABLE `transition` (
+	`id` TINYINT UNSIGNED NOT NULL KEY,
+	`from_relation_id` TINYINT UNSIGNED NOT NULL,
+	`to_relation_id` TINYINT UNSIGNED NOT NULL,
+	FOREIGN KEY (`from_relation_id`) REFERENCES `relation` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (`to_relation_id`) REFERENCES `relation` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE `change` (
+	`id`INT UNSIGNED NOT NULL AUTO_INCREMENT KEY,
+	`user_id` INT UNSIGNED NOT NULL,
+	`transition_id` TINYINT UNSIGNED NOT NULL,
+	`date` TIMESTAMP NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (`transition_id`) REFERENCES `transition` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+)
