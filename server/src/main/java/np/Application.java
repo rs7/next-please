64d0b4927de3bc4fd9791e2ad5522688a1a84b90
Roman Suslin <suslin@azoft.com>
2015-09-14 15:02:28 +1000

    Удалён неиспользуемый импорт (чистка кода)

--- server/src/main/java/np/Application.java
+++ server/src/main/java/np/Application.java
@@ -1,6 +1,5 @@
 package np;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
