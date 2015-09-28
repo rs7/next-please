5f28d9409006e861f8be410b16d63e82574fd577
rs7 <rs7xxx@gmail.com>
2015-09-28 19:57:13 +1000

    Добавлена поддержка amf сервером (resolve #21)
    Ответ сервера доступен в формате amf через Accept:application/x-amf и .amf соглашения

--- server/src/main/java/np/WebConfig.java
+++ server/src/main/java/np/WebConfig.java
@@ -3,7 +3,11 @@ package np;
 import java.util.List;
 
 import com.thoughtworks.xstream.XStream;
+
+import flex.messaging.io.MessageIOConstants;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.flex.http.AmfHttpMessageConverter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -17,13 +21,17 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-		configurer.defaultContentType(MediaType.APPLICATION_XML);
+		configurer
+			.defaultContentType(MediaType.APPLICATION_XML)
+			.mediaType("amf", MediaType.parseMediaType(MessageIOConstants.AMF_CONTENT_TYPE))
+		;
 	}
 
 	@Override
 	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
 		converters.add(createXmlHttpMessageConverter());
 		converters.add(new MappingJackson2HttpMessageConverter());
+		converters.add(new AmfHttpMessageConverter());
 	}
 
 	private HttpMessageConverter<Object> createXmlHttpMessageConverter() {
